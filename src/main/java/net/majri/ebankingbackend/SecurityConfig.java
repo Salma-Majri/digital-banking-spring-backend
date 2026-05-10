7d56c96cf8456310722c26e7d669c1afc35adf23
Salma Majri <salmamajri7@gmail.com>
2026-05-10 16:58:52 +0100

    feat: configure BCryptPasswordEncoder and In-Memory UserDetailsService with user1 and admin

--- src/main/java/net/majri/ebankingbackend/SecurityConfig.java
+++ src/main/java/net/majri/ebankingbackend/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +27,19 @@ public class SecurityConfig {
                 )
                 .httpBasic(hb -> hb.disable())
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(passwordEncoder.encode("12345")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("12345")).roles("USER", "ADMIN").build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
